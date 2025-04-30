5f24ea225922da493f498dfaad6cef78588b07c9
phufaphu <pphufaphu@gmail.com>
2025-05-01 00:21:56 +0700

    update: build config

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,4 +1,4 @@
-<script>
+<script >
 	let { showModal = $bindable(),showModal2 = $bindable(),showModal3 = $bindable(),showModal4 = $bindable(), header, children, random } = $props();
 
 	let dialog = $state(); // HTMLDialogElement
@@ -120,24 +120,6 @@
 		display: block;
 	}
 </style>
-<script>
-	let { showModal = $bindable(),showModal2 = $bindable(),showModal3 = $bindable(),showModal4 = $bindable(), header, children, random } = $props();
-
-	let dialog = $state(); // HTMLDialogElement
-
-	$effect(() => {
-		if (showModal) dialog.showModal();
-	});
-	$effect(() => {
-		if (showModal2) dialog.showModal();
-	});
-	$effect(() => {
-		if (showModal3) dialog.showModal();
-	});
-	$effect(() => {
-		if (showModal4) dialog.showModal();
-	});
-</script>
 
 <dialog
 	bind:this={dialog}
@@ -197,48 +179,3 @@
 		<!-- <button autofocus onclick={() => dialog.close()}>close modal</button> -->
 	</div>
 </dialog>
-
-
-<style>
-	dialog {
-		/* max-width: 32em;
-		/* border-radius: 0.2em; */
-		/* border: none;
-		padding: 20px;
-        margin: auto;
-        border-radius: 20px; */
-		margin: auto;
-		background: transparent;
-	}
-	dialog::backdrop {
-		background: rgba(0, 0, 0, 0.3);
-	}
-	dialog > div {
-		padding: 1em;
-	}
-	dialog[open] {
-		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
-	}
-	@keyframes zoom {
-		from {
-			transform: scale(0.95);
-		}
-		to {
-			transform: scale(1);
-		}
-	}
-	dialog[open]::backdrop {
-		animation: fade 0.2s ease-out;
-	}
-	@keyframes fade {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
-	button {
-		display: block;
-	}
-</style>
